e841f46b11d7f4ef62ea833ce1f74ea1748c7837
Dariusz Jędrzejczyk <dariusz.jedrzejczyk@broadcom.com>
2024-03-01 11:10:36 +0100

    Init project

---
+++ src/main/java/dev/jedrzejczyk/r2dbclab/R2dbcLabApplication.java
@@ -0,0 +1,13 @@
+package dev.jedrzejczyk.r2dbclab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jedrzejczyk/r2dbclab/R2dbcLabApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jedrzejczyk.r2dbclab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
